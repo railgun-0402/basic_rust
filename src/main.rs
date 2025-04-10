1f82b739cc63c5a911bb0b2f6f867488c32a4ab6
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2025-04-10 18:52:35 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+
+fn main() {
+    // 型推論
+    let x = 13;
+    println!("{}", x);
+
+    // 型指定
+    let x: f64 = 3.14159;
+    println!("{}", x);
+
+    // 宣言後に初期化
+    let x;
+    x = 0;
+    println!("{}", x);
+
+    basic_variable();
+}
+
+fn immute() {
+    let mut x = 42;
+    println!("{}", x);
+    x = 13;
+    println!("{}", x);
+}
+
+pub fn basic_variable() {
+    let x = 12; // デフォルトでは i32
+    let a = 12u8;
+    let b = 4.3; // デフォルトでは f64
+    let c = 4.3f32;
+    let bv = true;
+    let t = (13, false);
+    let sentence = "hello world!";
+    println!(
+        "{} {} {} {} {} {} {} {}",
+        x, a, b, c, bv, t.0, t.1, sentence
+    );
+}
